05299c674cc9dca59be670c66664c54598f0a047
Naiket08 <patelnaiket08@gmail.com>
2022-10-30 01:43:30 -0400

    Dashboard menu item added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
+import { DashboardComponent } from './Components/home/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'Home', component: HomeComponent }
+  {
+    path: 'Home', component: HomeComponent,
+    // children: [
+    //   { path: '', component: DashboardComponent }
+    // ]
+  }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,22 @@ import { providePerformance,getPerformance } from '@angular/fire/performance';
 import { AngularFireModule } from '@angular/fire/compat';
 import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './Components/home/dashboard/dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
+    DashboardComponent,
 
   ],
   imports: [
@@ -26,11 +36,19 @@ import { HomeComponent } from './Components/home/home.component';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule,
+    NgbCollapseModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     providePerformance(() => getPerformance()),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
